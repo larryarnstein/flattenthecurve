4af08d9e56decf69edf82b6ef302575d75f860e6
Larry Arnstein <larrya@gmail.com>
2020-03-16 21:28:54 -0700

    added random number seed for repeatability and set initial patient count to 1

--- theCurve.py
+++ theCurve.py
@@ -7,15 +7,18 @@ recovery = 21 #median days to recover median
 terminal = 14 #median days to die if not recovering
 transmit = 5 #median days to transmit infection
 start_of_lock_down = 40
+max_lookahead = 60 #the extra space we need in the arrays to capture data that happens after the end of the simulation
+lock_down_effectiveness = 0.9 #rate at which R0 declines after the start of lockdown
 
-R0 = np.zeros([10*duration])
-sick = np.zeros([10*duration])
-recovered = np.zeros([10*duration])
-dead = np.zeros([10*duration])
-new = np.zeros([10*duration])
+np.random.seed(1)
+R0 = np.zeros([max_lookahead + duration])
+sick = np.zeros([max_lookahead + duration])
+recovered = np.zeros([max_lookahead + duration])
+dead = np.zeros([max_lookahead + duration])
+new = np.zeros([max_lookahead + duration])
 
 # need to add patient zero fate to the other arrays
-sick[0] = 10 
+sick[0] = 1  #initial number of sick people to get things going. Can start with 1
 new[0] = 1
 R0[0] = 2.1 #this was the initial reported R0, this should decline with aggressive social constraints
 
@@ -27,31 +30,27 @@ for d in days :
         dead[d] = dead[d] + dead[d-1]
         recovered[d] = recovered[d] + recovered[d-1]
         if (d > start_of_lock_down) :
-            newR0 = R0[d-1] * 0.98
+            newR0 = R0[d-1] * 0.95
             R0[d] = max(0.5, newR0)
         else :
             R0[d] = R0[d-1]
     newly_infected = 0
     for patient in range(int(new[d])) :
-        newly_infected = int(newly_infected + max(0,np.round(np.random.normal(R0[d],0.5))))
+        newly_infected = int(newly_infected + min(max_lookahead,max(0,np.round(np.random.normal(R0[d],0.5)))))
     for n in range(int(newly_infected)) :
         outcome = np.random.uniform()
-        date_infected = int(d + np.round(max(1,np.random.normal(transmit,7))))
+        date_infected = int(d + np.round(min(max_lookahead, max(1,np.random.normal(transmit,7)))))
         new[date_infected] = new[date_infected] + 1
         if (outcome < survival) :
-            time_to_recover = int(np.round(max(1,np.random.normal(recovery,7))))
+            time_to_recover = int(np.round(min(max_lookahead, max(1,np.random.normal(recovery,7)))))
             recovered[date_infected+time_to_recover] = recovered[date_infected+time_to_recover] + 1
             sick[date_infected+time_to_recover] = sick[date_infected+time_to_recover]-1
         else :
-            time_to_die = int(np.round(max(1,np.random.normal(terminal,3))))
+            time_to_die = int(np.round(min(max_lookahead,max(1,np.random.normal(terminal,3)))))
             dead[date_infected+time_to_die] = dead[date_infected+time_to_die]+1
             sick[date_infected+time_to_die] = sick[date_infected+time_to_die]-1
     
-    
-#print("sick",sick)
-#print("new",new)
-#print("recovered", recovered)
-print("dead", R0[0:duration])
+
 
 fig, axs = plt.subplots(4)
 plt.ylim(bottom=0)
